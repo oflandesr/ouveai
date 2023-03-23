929c87bba40b2cb1064aabc126dfeb510ed240e9
Oflandes Rodrigues <oflandes.silva@markway.com.br>
2023-03-23 15:54:25 -0300

    Melhorias na logica

--- app/src/main/java/br/com/ouveai/MainActivity.kt
+++ app/src/main/java/br/com/ouveai/MainActivity.kt
@@ -2,14 +2,17 @@ package br.com.ouveai
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.AudioFormat
+import android.media.AudioRecord
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import kotlin.math.log10
+import java.lang.Math.log10
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mediaRecorder: MediaRecorder
@@ -18,10 +21,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mediaRecorder = MediaRecorder()
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+        mediaRecorder.setOutputFile("/dev/null")
     }
 
     override fun onStart() {
         super.onStart()
+        // Solicita permissão do usuário
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
             != PackageManager.PERMISSION_GRANTED
         ) {
@@ -31,13 +39,6 @@ class MainActivity : AppCompatActivity() {
                 1
             )
         }
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-        mediaRecorder.setOutputFile("/dev/null")
-        mediaRecorder.prepare()
-        mediaRecorder.start()
-        updateDecibelTextView()
     }
 
     override fun onRequestPermissionsResult(
@@ -49,6 +50,9 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == 1) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permissão concedida, inicie a gravação do áudio
+                mediaRecorder.prepare()
+                mediaRecorder.start()
+                updateDecibelTextView()
             } else {
                 // Permissão negada, não é possível gravar o áudio
             }
@@ -57,18 +61,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateDecibelTextView() {
         val amplitude = mediaRecorder.maxAmplitude.toDouble()
-        val db = 20 * log10(amplitude / 32767.0)
+        val db = 20 * kotlin.math.log10(amplitude / 32767.0)
         val decibelText = String.format("%.1f dB", db)
         val textView = findViewById<TextView>(R.id.textViewDecibel) as TextView
         textView.text = decibelText
         Handler().postDelayed({ updateDecibelTextView() }, 100)
     }
 }
-
-
-/*class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}*/
